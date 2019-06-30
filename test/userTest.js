2e3c067b6e55a1a75c22a81a38865dd35c89bca4
Jonathan Espiard <jonathan.espiard@epita.fr>
2019-06-29 22:19:29 -0400

    [ADD] Added to return list of users to chat with

--- test/userTest.js
+++ test/userTest.js
@@ -25,4 +25,23 @@ describe('User test', function () {
       });
     });
   });
+
+  it('should return an array of users', function (done) {
+    // Set up client1 connection
+    let client = io.connect(socketUrl, options);
+
+    client.on('connect', function () {
+      client.emit('USER_FRIENDS', 1);
+      client.on('FRIENDS', data => {
+        expect(data[0].userId).to.equal(2);
+        expect(data[0].username).to.equal('toto');
+        expect(data[0].email).to.equal('toto@gmail.com')
+        expect(data.length).to.be.above(0);
+        expect(data.some(elt => elt.userId == 1)).to.equal(false);
+        expect(data.some(elt => elt.username == "Jonathan")).to.equal(false);
+        client.disconnect();
+        done();
+      });
+    });
+  });
 });
