8751361d24e317972e35d3e2a032602922d4bc3a
Jonathan Espiard <jonathan.espiard@epita.fr>
2019-07-03 16:39:48 -0400

    [ADD] Working on the mainpage design/frontend

--- frontend/src/layouts/MainPage.js
+++ frontend/src/layouts/MainPage.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Login from '../components/login/Login';
 import Inscription from '../components/register/Register';
-import Chat from'../components/chat/Chat';
+import Chat from '../components/chat/Chat';
 import Menu from './Menu';
+import '../css/project.css'
 
 const mapStateToProps = state => ({
     username: state.user.username,
@@ -12,12 +13,19 @@ const mapStateToProps = state => ({
     connexionDemand: state.user.connexionDemand
 });
 
-const MainPage = ({ dispatch, connexionDemand, registerDemand}) => (
+const MainPage = ({ dispatch, connexionDemand, registerDemand }) => (
     <div id="wrapper">
         <Menu />
-        {registerDemand && !connexionDemand && <Inscription dispatch={dispatch} />}
-        {connexionDemand && !registerDemand && <Login dispatch={dispatch} />}
-        {!connexionDemand && !registerDemand && <Chat/>}
+        <body>
+        <main>
+            {registerDemand && !connexionDemand && <Inscription dispatch={dispatch} />}
+            {connexionDemand && !registerDemand && <Login dispatch={dispatch} />}
+            {!connexionDemand && !registerDemand && <Chat />}
+            <h1>Your Workspace</h1>
+            <p>You currently have no projects to display</p>
+            <a href="#" class="cta"> Create your Project </a>
+        </main>
+        </body>
     </div>
 );
 
