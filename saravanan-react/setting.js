fb2d5aaa6caa1623ff662d4311485408edc65e5f
Jonathan Espiard <jonathan.espiard@epita.fr>
2019-07-09 17:57:03 -0400

    [DELETE] Removed unused files

--- saravanan-react/setting.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-class Settings extends Component {
-  render() {
-    return <div className="container-fluid"> Do your setting here </div>;
-  }
-}
-export default Settings;
