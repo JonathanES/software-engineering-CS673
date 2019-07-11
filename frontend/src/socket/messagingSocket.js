fce2867b605eeee8983b6558ea4dedb48ce14df0
Jonathan Espiard <jonathan.espiard@epita.fr>
2019-07-11 11:51:47 -0400

    [UPDATE] Corrected test for direct messaging

--- frontend/src/socket/messagingSocket.js
+++ frontend/src/socket/messagingSocket.js
@@ -3,12 +3,12 @@
  */
 import {socket} from './config'
   function sendMessage(userId, receiverId, message, cb){
-    socket.on('SEND_MESSAGE', data => cb(null, data) );
+    socket.on('SEND_MESSAGE', data => {console.log("send " + data); cb(null, data)} );
     socket.emit('USER_SEND_MESSAGE', userId, receiverId, message);
   }
 
   function getMessage(userId, receiverId, cb){
-    socket.on('SEND_MESSAGE', data => cb(null, data) );
+    socket.on('SEND_MESSAGE', data => {console.log("get " + data); cb(null, data)} );
     socket.emit('USER_GET_MESSAGE', userId, receiverId);
   }
 
--- test/tests/chatTest.js
+++ test/tests/chatTest.js
@@ -18,20 +18,20 @@ describe('Messaging test', function () {
     // Set up client1 connection
     let client = io.connect(socketUrl, options);
 
-    client.on('connect', function () {
+    client.on('connect', async function () {
 
-      client.emit('USER_LOGIN', 'jonathan.espiard@gmail.com', 'pwd');
       client.emit('USER_LOGIN', 'toto@gmail.com', 'pwd');
-
-      // Emit event when all clients are connected.
-      client.emit('USER_SEND_MESSAGE', userId, receiverId, 'test');
-      client.on('SEND_MESSAGE', data => {
-        console.log("test " + data);
-        expect(data[data.length - 1].senderId).to.equal(userId);
-        expect(data[data.length - 1].receiverId).to.equal(receiverId);
-        expect(data[data.length - 1].Message).to.equal('test');
-        client.disconnect();
-        done();
+      client.on('LOGIN', data => {
+        expect(data.username).to.equal('toto');
+        expect(data.email).to.equal('toto@gmail.com');
+        client.emit('USER_SEND_MESSAGE', userId, receiverId, 'test');
+        client.on('SEND_MESSAGE', async (data) => {
+          expect(data[data.length - 1].senderId).to.equal(userId);
+          expect(data[data.length - 1].receiverId).to.equal(receiverId);
+          expect(data[data.length - 1].Message).to.equal('test');
+          client.disconnect();
+          done();
+        });
       });
     });
   });
@@ -42,19 +42,21 @@ describe('Messaging test', function () {
 
     client.on('connect', function () {
 
-      client.emit('USER_LOGIN', 'jonathan.espiard@gmail.com', 'pwd');
       client.emit('USER_LOGIN', 'toto@gmail.com', 'pwd');
-      // Emit event when all clients are connected.
-      client.emit('USER_GET_MESSAGE', userId, receiverId);
-      client.on('SEND_MESSAGE', data => {
-        expect(data.length).to.be.above(0);
-        expect(data[data.length - 1].senderId).to.equal(userId);
-        expect(data[data.length - 1].receiverId).to.equal(receiverId);
-        db.query('DELETE FROM DirectMessaging WHERE Message = ? AND senderID = ? AND receiverID = ?', ['test', userId, receiverId], (error) => {
-          if (error) throw error;
-          client.disconnect();
-          done();
-        })
+      client.on('LOGIN', data => {
+        expect(data.username).to.equal('toto');
+        expect(data.email).to.equal('toto@gmail.com');
+        client.emit('USER_GET_MESSAGE', userId, receiverId);
+        client.on('SEND_MESSAGE', data => {
+          expect(data.length).to.be.above(0);
+          expect(data[data.length - 1].senderId).to.equal(userId);
+          expect(data[data.length - 1].receiverId).to.equal(receiverId);
+          db.query('DELETE FROM DirectMessaging WHERE Message = ? AND senderID = ? AND receiverID = ?', ['test', userId, receiverId], (error) => {
+            if (error) throw error;
+            client.disconnect();
+            done();
+          });
+        });
       });
     });
   });
