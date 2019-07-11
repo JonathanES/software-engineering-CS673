f978d2cec3a000c3fe4920bf332b28132e2b1987
Jonathan Espiard <jonathan.espiard@epita.fr>
2019-07-11 15:49:15 -0400

    [ADD] Corrected the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import MainPage from './layouts/MainPage'
-import "./css/main_reg.css";
-import "./css/main_login.css";
 
 const App = () => (
   <Provider store={store}>
