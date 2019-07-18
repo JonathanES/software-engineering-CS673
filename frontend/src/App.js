083003781a47955f2e4e05866c22518ea1cdc4d4
Jonathan Espiard <jonathan.espiard@epita.fr>
2019-07-18 18:16:03 -0400

    [ADD] Working on the cookies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import MainPage from './layouts/MainPage'
+import { CookiesProvider } from 'react-cookie';
+
 
 const App = () => (
-  <Provider store={store}>
-    <MainPage />
-  </Provider>
+  <CookiesProvider>
+    <Provider store={store}>
+      <MainPage />
+    </Provider>
+  </CookiesProvider>
 );
 
 export default App;
